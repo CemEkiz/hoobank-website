b55e580eb591bac0c525f37a85dcf7fda63494ee
Cem Ekiz <ekizcem.pro@gmail.com>
2022-08-19 18:07:29 +0200

    fix: Hero section issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
 						The Next <br className='hidden sm:block' />{' '}
 						<span className='text-gradient'>Generation</span>{' '}
 					</h1>
-					<div className='ss:flex hidden md:mr-4 mr-0'>
+					<div className='sm:flex hidden md:mr-4 mr-0 ml-10'>
 						<GetStarted />
 					</div>
 				</div>
@@ -45,20 +45,24 @@ const Hero = () => {
 			</div>
 
 			<div
-				className={`flex1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+				className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
 			>
+				{/* FIXME: When we are around 1000-1200px and on large screen the image doesnt look good */}
 				<img
 					src={robot}
 					alt='billing'
 					className='w-[100%] h-[100%] relative z-[5]'
 				/>
-				<div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
-				<div className='absolute z-[1] w-[80%] h-[80%] rounded-full top-0 bottom-40 white__gradient'></div>
-				<div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'></div>
 
-				<div className={`ss:hidden ${styles.flexCenter}`}>
-					<GetStarted />
-				</div>
+				{/* gradient start */}
+				<div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+				<div className='absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40' />
+				<div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+				{/* gradient end */}
+			</div>
+
+			<div className={`sm:hidden ${styles.flexCenter}`}>
+				<GetStarted />
 			</div>
 		</section>
 	);
