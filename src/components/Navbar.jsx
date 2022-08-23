36e7fd6d70a4039a7cff11389031b84f5326ffa6
Cem Ekiz <ekizcem.pro@gmail.com>
2022-08-23 08:15:56 +0200

    fix: z-index mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,21 +7,16 @@ const Navbar = () => {
 
 	return (
 		<nav className='navbar w-full flex py-6 justify-between items-center'>
-			<img
-				src={logo}
-				alt='hoobank'
-				className='w-[124px] h-[32px]'
-			/>
+			<img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
 
 			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
 				{navLinks.map((nav, index) => (
 					<li
 						key={nav.id}
 						className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
-							index === navLinks.length - 1
-								? 'mr-0'
-								: 'mr-10'
-						}`}>
+							index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+						}`}
+					>
 						<a href={`#${nav.id}`}>{nav.title}</a>
 					</li>
 				))}
@@ -32,15 +27,14 @@ const Navbar = () => {
 					src={toggle ? close : menu}
 					alt='menu'
 					className='w-[28px] h-[28px] object-contain'
-					onClick={() =>
-						setToggle((prev) => !prev)
-					}
+					onClick={() => setToggle((prev) => !prev)}
 				/>
 
 				<div
 					className={`${
 						toggle ? 'flex' : 'hidden'
-					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[240px] rounded-xl sidebar`}>
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[240px] rounded-xl sidebar z-50`}
+				>
 					<ul className='list-none flex flex-col justify-end items-center flex-1'>
 						{navLinks.map((nav, index) => (
 							<li
@@ -49,10 +43,9 @@ const Navbar = () => {
 									index === navLinks.length - 1
 										? 'mb-0'
 										: 'mb-4'
-								}`}>
-								<a href={`#${nav.id}`}>
-									{nav.title}
-								</a>
+								}`}
+							>
+								<a href={`#${nav.id}`}>{nav.title}</a>
 							</li>
 						))}
 					</ul>
