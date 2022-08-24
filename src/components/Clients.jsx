04fb8b85e00e2e12d6a6a449f8b48db4007f74bd
Cem Ekiz <ekizcem.pro@gmail.com>
2022-08-24 11:02:55 +0200

    fix: update gap between trusted clients and cta section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,8 +2,10 @@ import { clients } from '../constants';
 import styles from '../style';
 
 const Clients = () => (
-	<section className={`${styles.flexCenter} my-4`}>
-		<div className={`${styles.flexCenter} flex-wrap w-full`}>
+	<section className={`${styles.flexCenter} my-4 mb-20 md:mb-40`}>
+		<div
+			className={`${styles.flexCenter} flex-wrap w-full gap-x-10 gap-y-5 sm:gap-y-0`}
+		>
 			{clients.map((client) => (
 				<div
 					key={client.id}
